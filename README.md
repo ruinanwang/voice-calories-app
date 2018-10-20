a50a0f6d8c486321b11e639d3607537efb808033
JingboWang1997 <wjbjimmy@gmail.com>
2018-10-20 14:54:14 -0400

    Added frontend and connected it to backend

--- README.md
+++ README.md
@@ -10,24 +10,31 @@ Django<br/>
 Django Restful API Fraimwork<br/>
 `pip install djangorestframework`<br/>
 `pip install markdown       # Markdown support for the browsable API`<br/>
-`pip install django-filter  # Filtering support`<br/>
 
-#### Run the server
+#### Run the backend server
 
 Use the environment:<br/>
-`cd hackgtEnv/`<br/>
 `source bin/activate`<br/>
 
 Serve:
 `python manage.py runserver`<br/>
 
-Run the database: <br/>
+Sync the database: <br/>
 `python manage.py migrate`
 
 Make new component:<br/>
 `Python manage.py startapp product`<br/>
 `Python manage.py makemigrations`<br/>
 `Python manage.py migrate`<br/>
+
+#### Run the frontend server
+
+Use the environment:<br/>
+`cd frontend/`<br/>
+`npm install -g expo-cli`<br/>
+
+Serve:
+`npm start`<br/>
 
 
 
---
+++ frontend/components/calorieFetch.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { Text, View, Button } from 'react-native';
+
+class CalorieFetch extends Component {
+    constructor() {
+        super();
+        this.state = {
+            calorie: []
+        }
+
+        this.fetchCalorie = this.fetchCalorie.bind(this)
+    }
+
+    componentDidMount() {
+        // this.fetchCalorie("one cup of coffee")
+    }
+
+    fetchCalorie = (calorieString) => {
+        fetch('https://radiant-brook-49591.herokuapp.com/getCalories/', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(calorieString)})
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data.calorie);
+            this.setState({calorie: data.calorie});
+            console.log(this.state.calorie);
+            return data;
+        })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <View>
+                    <Text>hello</Text>
+                    <Text>{this.state.calorie}</Text>
+                </View>
+                <Button
+                    onPress={ () => { this.fetchCalorie("one bowl of rice") } }
+                    title="Get Calories"
+                />
+            </React.Fragment>
+        )
+    }
+}
+
+export default CalorieFetch;
