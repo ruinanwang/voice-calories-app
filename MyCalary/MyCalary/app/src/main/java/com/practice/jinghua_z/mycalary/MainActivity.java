2d1cfb6bcdd39d155d560a4ac7a25931c92717fc
Ruinan <nancywng@sina.com>
2018-10-20 20:26:01 -0400

    added api call to android

--- MyCalary/MyCalary/app/src/main/java/com/practice/jinghua_z/mycalary/MainActivity.java
+++ MyCalary/MyCalary/app/src/main/java/com/practice/jinghua_z/mycalary/MainActivity.java
@@ -14,6 +14,16 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+//API call import
+import com.practice.jinghua_z.mycalary.httpService.GetCaloriesService;
+import com.practice.jinghua_z.mycalary.requestModel.GetCaloriesBodyString;
+import com.practice.jinghua_z.mycalary.responseModel.GetCaloriesResponse;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     protected static final int RESULT_SPEECH = 1;
@@ -21,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtText;
     private Button voice_button, voice_clear;
 
+    private static final String baseUrl = "http://10.0.2.2:8000/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
 //        voice_clear = (Button) findViewById(R.id.voice_clear);
         txtText = (TextView) findViewById(R.id.textView2);
 
+        getCalories("one cup of coffee");
 
 
 //        voice_clear.setOnClickListener(new View.OnClickListener() {
@@ -81,6 +93,39 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    public void getCalories(String voiceInput) {
+
+        Log.d("nancy", "inside getCalories()");
+
+        Retrofit getCaloriesRetrofit = new Retrofit.Builder().
+                baseUrl(baseUrl).
+                addConverterFactory(GsonConverterFactory.create()).
+                build();
+
+        GetCaloriesService getCaloriesService = getCaloriesRetrofit.create(GetCaloriesService.class);
+        GetCaloriesBodyString getCaloriesBodyString = new GetCaloriesBodyString(voiceInput);
+        Call<GetCaloriesResponse> getCaloriesResponse = getCaloriesService.getCalories(getCaloriesBodyString);
+
+        Log.d("nancy bodyString", getCaloriesBodyString.getVoiceInput());
+
+        getCaloriesResponse.enqueue(new Callback<GetCaloriesResponse>() {
+            @Override
+            public void onResponse(Call<GetCaloriesResponse> call, Response<GetCaloriesResponse> response) {
+                Log.d("nancy", response.toString());
+                if (response.body() != null) {
+                    Log.d("nancy", response.toString());
+                    int calorie = response.body().getCalorie();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GetCaloriesResponse> call, Throwable t) {
+                Log.d("nancy error", t.toString());
+            }
+        });
+
     }
 
 
--- product/views.py
+++ product/views.py
@@ -24,4 +24,9 @@ from rest_framework.decorators import api_view
 @api_view(['POST'])
 def getCalories(request):
     data = text.retrieveUnitItemFromText(request.data)
+    print(data)
     return Response({"calorie":data[0][0]})
+    # body_unicode = request.body.decode('utf-8')
+    # body = json.loads(body_unicode)
+    # data = text.retrieveUnitItemFromText(body)
+    # return Response({"calorie":data})
